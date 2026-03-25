16b3f0be1351a8cf2124c726539fe2b41c3f7917
ilkash <126604951+ilkash@users.noreply.github.com>
2026-03-25 17:53:02 +0200

    fix

--- src/app/(root)/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function RootPage() {
-  console.log();
-  return null;
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
+import { Open_Sans, Fira_Code } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+
 const openSans = Open_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
+  variable: "--font-open-sans",
 });
 
-// const firaCode = Fira_Code({
-//   subsets: ["latin"],
-//   weight: ["400", "500"],
-// });
+const firaCode = Fira_Code({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  variable: "--font-fira-code",
+});
 
 export const metadata: Metadata = {
   title: "Admin Panel",
@@ -24,8 +27,8 @@ export default function RootLayout({
 }) {
   return (
     <AuthProvider>
-      <html lang="en">
-        <body className={openSans.className}>{children}</body>
+      <html lang="en" className={`${openSans.variable} ${firaCode.variable}`}>
+        <body className="font-sans">{children}</body>
       </html>
     </AuthProvider>
   );
