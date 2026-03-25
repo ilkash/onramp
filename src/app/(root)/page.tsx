5756dbe5820fbf192b0585364ffb81b07167d4ff
ilkash <126604951+ilkash@users.noreply.github.com>
2026-03-25 17:40:53 +0200

    Update page.tsx

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,3 @@
-"use client";
-
-import HomePageClient from "@/features/HomePage";
-
-const HomePage = () => {
-  return <HomePageClient />;
-};
-
-export default HomePage;
+export default function RootPage() {
+  return null;
+}
